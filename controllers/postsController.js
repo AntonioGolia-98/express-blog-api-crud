9676a360935c1e9cb8f99c6b16bfcfb7a59616f3
AntonioGolia-98 <antoniog98@icloud.com>
2026-04-06 17:31:53 +0200

    controllers

---
+++ controllers/postsController.js
@@ -0,0 +1,27 @@
+const posts = require("./../data/postList");
+
+function index(req, res) {
+
+}
+
+function show(req, res) {
+
+}
+
+function store(req, res) {
+
+}
+
+function update(req, res) {
+
+}
+
+function modify(req, res) {
+
+}
+
+function destroy(req, res) {
+
+}
+
+module.exports = { index, show, store, update, modify, destroy };
