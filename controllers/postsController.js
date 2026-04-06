229871aed139aa8e3bae7ed576aec0214f9fdd90
AntonioGolia-98 <antoniog98@icloud.com>
2026-04-06 17:40:03 +0200

    index filter

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,7 +1,14 @@
 const posts = require("./../data/postList");
 
 function index(req, res) {
+    let filteredPosts = posts;
 
+    if (req.query.tags) {
+        filteredPosts = posts.filter(
+            post => post.tags.includes(req.query.tags)
+        );
+    }
+    res.json(filteredPosts)
 }
 
 function show(req, res) {
@@ -9,15 +16,16 @@ function show(req, res) {
 }
 
 function store(req, res) {
+    res.send("creazione nuovo post")
 
 }
 
 function update(req, res) {
-
+    res.send("modifica del post" + req.params.id)
 }
 
 function modify(req, res) {
-
+    res.send("modifica paraziale del post" + req.params.id)
 }
 
 function destroy(req, res) {
