d9d3e718606afd9eccdcc675d3fcc3f047f0276b
AntonioGolia-98 <antoniog98@icloud.com>
2026-04-06 17:46:27 +0200

    implemetazione destroy

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -39,7 +39,19 @@ function modify(req, res) {
 }
 
 function destroy(req, res) {
+    const id = parseInt(req.params.id);
+    const post = posts.find((post) => post.id === id)
 
+    if (!post) {
+        res.status(404)
+        return res.json({
+            error: 'not found',
+            messaggio: 'oggetto non trovato'
+        })
+    }
+    posts.splice(posts.indexOf(post), 1);
+    res.send('Cancellato il post ' + id);
+    res.status(204)
 }
 
 module.exports = { index, show, store, update, modify, destroy };
